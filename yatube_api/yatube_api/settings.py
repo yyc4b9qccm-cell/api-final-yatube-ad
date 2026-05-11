531f72a98c1fe0e24d94eed69dc3f93ea5890635
yyc4b9qccm-cell <yyc4b9qccm@privaterelay.appleid.com>
2026-05-11 21:45:56 +0500

    Update settings.py

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -94,9 +94,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
 }
-  #  'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-  #  'PAGE_SIZE': 10,
-}
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
