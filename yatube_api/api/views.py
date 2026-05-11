294e6f3e161d476d325b79f9d5f7e0f22f13794d
yyc4b9qccm-cell <yyc4b9qccm@privaterelay.appleid.com>
2026-05-11 21:32:02 +0500

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,18 +1,18 @@
 from rest_framework import viewsets, filters
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import ValidationError
-from posts.models import Post, Group, Follow
+from posts.models import Post, Group, Comment, Follow
 from .serializers import (
     PostSerializer, GroupSerializer, CommentSerializer, FollowSerializer
 )
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -25,7 +25,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
@@ -46,6 +46,11 @@ class FollowViewSet(viewsets.ModelViewSet):
         return Follow.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        if self.request.user == serializer.validated_data['following']:
+        following = serializer.validated_data['following']
+        if self.request.user == following:
             raise ValidationError('Нельзя подписаться на самого себя')
+        if Follow.objects.filter(
+            user=self.request.user, following=following
+        ).exists():
+            raise ValidationError('Вы уже подписаны на этого пользователя')
         serializer.save(user=self.request.user)
