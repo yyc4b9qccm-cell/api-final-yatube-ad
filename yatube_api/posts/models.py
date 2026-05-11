aca8dd01ab05193c714e29ef5015daa0064b8c22
yyc4b9qccm-cell <yyc4b9qccm@privaterelay.appleid.com>
2026-05-11 20:24:02 +0500

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -24,3 +24,21 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    following = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        unique_together = ('user', 'following')
+
+    def __str__(self):
+        return f'{self.user} подписан на {self.following}'
